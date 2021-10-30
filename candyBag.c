3fe573692957602c7c997f821a7c1901aac436d6
Mohamad Alabudi <80290328+malabudi@users.noreply.github.com>
2021-10-29 22:50:54 -0400

    Added an option to eat the candy, and download the amount of each candy in a text file
    The user can now eat candy from the candy bag by picking which candy and how many they want to eat. Also the user can download a text file of how much candy they have, and when they launch the program again they can load it back into the program and it will remember how much candy there was for each candy.

--- candyBag.c
+++ candyBag.c
@@ -5,15 +5,18 @@ void getCandyCounts(char **candyBagPtrs, int numCandies[]);
 char menu();
 void displayCandy(char **candyBagPtrs, int numCandies[]);
 void displayIndividualCandy(char **candyBagPtrs, int numCandies[]);
+void getCandyToEat(char **candyBagPtrs, int numCandies[], int *candyChoicePtr, int *eatAmount);
+void downloadFile(int numCandies[]);
 
 
 int main()
 {
     // Initialize an array of strings holding the candy name, and another array to hold the amount of each candy
     char *candyBag[] = {"Snickers", "Hersheys", "Skittles", "Reeses", "Kit Kat bar"};
-    int numCandies[5];
+    int numCandies[5], candyChoice, eatAmount;
     char choice;
 
+    // Grab the amount of candy the user has in each bag
     getCandyCounts(candyBag, numCandies);
 
     while (choice != 'E')
@@ -30,9 +33,19 @@ int main()
                 displayIndividualCandy(candyBag, numCandies);
                 break;
             case 'C':
+                getCandyToEat(candyBag, numCandies, &candyChoice, &eatAmount);
+
+                // Update the amount of candy after it has been eaten in the numCandies array
+                numCandies[candyChoice - 1] -= eatAmount;
                 break;
             case 'D':
-                printf("\nWIP, sorry!");
+                downloadFile(numCandies);
+                break;
+            case 'E':
+                printf("\nClosing candy bag program.");
+                break;
+            default:
+                printf("\nInvalid input, please select a valid letter.");
                 break;
         }
     }
@@ -44,11 +57,55 @@ int main()
 // User defined functions
 void getCandyCounts(char **candyBagPtrs, int *numCandies)
 {
-    // Use a loop to ask the user what candy they would like to collect, and save the answer in the numCandies array
-    for (int i = 0; i < 5; i++)
+    int choice, i;
+    FILE *candyBagFile;
+    char fileName[50];
+
+    // Ask the user if they have a file that they would like to load into the program or manually enter that data by choosing from a menu
+    printf("\nWelcome to the candy bag program, how would you like to load your data?");
+    printf("\n1 - Manually enter the amount of each candy");
+    printf("\n2 - Load data for the candy bag from a text file\n");
+    scanf("\n%d", &choice);
+
+    // Validate menu entry
+    while (choice < 1 || choice > 2)
     {
-        printf("\nEnter the quantity of %s you collected: ", candyBagPtrs[i]);
-        scanf("\n%d", &numCandies[i]);
+        printf("\nInvalid input, please select a valid option (1-2) from the menu");
+
+        printf("\n1 - Manually enter the amount of each candy");
+        printf("\n2 - Load data for the candy bag from a text file\n");
+        scanf("\n%d", &choice);
+    }
+
+    switch(choice)
+    {
+        case 1:
+            // Use a loop to ask the user what candy they would like to collect, and save the answer in the numCandies array
+            for (int i = 0; i < 5; i++)
+            {
+                printf("\nEnter the quantity of %s you collected: ", candyBagPtrs[i]);
+                scanf("\n%d", &numCandies[i]);
+            }
+            break;
+        case 2:
+            // Ask the user for the file name and see if there is a match
+            printf("\nPlease enter the name of your txt file (include .txt at the end) ");
+            scanf("\n%s", fileName);
+
+            while ((candyBagFile = fopen(fileName, "r")) == NULL)
+            // If the user enters an invalid (null) file name let them enter a valid one
+            {
+                printf("\nError! %s could not be found.", fileName);
+
+                printf("\nPlease enter the name of your txt file (include .txt at the end) ");
+                scanf("\n%s", fileName);
+            }
+
+            // File found, begin reading each line in the text file and assign it to the numCandies arrau
+            for (i = 0; i < 5; i++)
+                fscanf(candyBagFile, "\n%d", &numCandies[i]);
+
+            break;
     }
 }
 
@@ -62,7 +119,7 @@ char menu()
     printf("\nA: Display total count of all candy\n");
     printf("\nB: Display total count of a specific type of candy\n");
     printf("\nC: Eat some candy\n");
-    printf("\nD: (WIP) Read saved candy from a file\n");
+    printf("\nD: Save the candy bag into a text file\n");
     printf("\nE: Quit\n\n");
 
     scanf("\n%c", &menuChoice);
@@ -89,7 +146,7 @@ void displayCandy(char **candyBagPtrs, int numCandies[])
 
 void displayIndividualCandy(char **candyBagPtrs, int numCandies[])
 {
-    char candyChoice[20];
+    char candyChoice[50];
     int i;
     int isCandyAvail = 0; // Check if the candy that the user typed in is found in the string array
 
@@ -114,4 +171,69 @@ void displayIndividualCandy(char **candyBagPtrs, int numCandies[])
         if (isCandyAvail == 0)
             printf("\n%s is not in your bag! Please try again!", candyChoice);
     }
+}
+
+
+void getCandyToEat(char **candyBagPtrs, int numCandies[], int *candyChoicePtr, int *eatAmountPtr)
+{
+    int i, candyChoice, eatAmount;
+
+    // Display menu to let the user choose (1-5) which candy they want to eat
+    for (i = 0; i < 5; i++)
+        printf("\n%d:   %s", i + 1, candyBagPtrs[i]);
+
+    printf("\nWhich candy would you like to eat (1-5): ");
+    scanf("\n%d", &candyChoice);
+
+    // Make sure candyChoice is (1-5)
+    while (candyChoice < 1 || candyChoice > 5)
+    {
+        printf("\nInvalid entry, please enter between 1-5");
+
+        printf("\nWhich candy would you like to eat (1-5): ");
+        scanf("\n%d", &candyChoice);
+    }
+
+    // Ask how much of that candy they would like to eat and validate that data
+    printf("\nHow many would you like to eat? ");
+    scanf("\n%d", &eatAmount);
+
+    while (eatAmount > numCandies[candyChoice - 1] || eatAmount < 0)
+    {
+        if (eatAmount <= 0)
+            printf("\nYou can not eat a negative amount of %s! Please enter a valid amount", candyBagPtrs[candyChoice - 1]);
+        else
+            printf("\nThere is not enough %s! Please enter a valid amount", candyBagPtrs[candyChoice - 1]);
+
+        printf("\nHow many would you like to eat? ");
+        scanf("\n%d", &eatAmount);
+    }
+
+    // assign the candy choice and amount of candy the user wants to eat after validation
+    *candyChoicePtr = candyChoice;
+    *eatAmountPtr = eatAmount;
+}
+
+
+void downloadFile(int numCandies[])
+{
+    FILE *candyBagFile;
+    char fileName[50];
+    int i;
+
+    // Ask the user what would they like to name the file before they download it
+    printf("\nWhat would you like to name your file? (make sure the name ends in .txt) ");
+    scanf("\n%s", fileName);
+
+    // Download the file name if it does not already exist, otherwise it will overwrite
+    candyBagFile = fopen(fileName, "w");
+
+    // Start writing the candy name in one line, and its amount in the line after, and repeat
+    for (i = 0; i < 5; i++)
+        fprintf(candyBagFile, "%d\n", numCandies[i]);       // Amount in bag per candy written in file
+
+    // Close the file
+    fclose(candyBagFile);
+
+    printf("\nFile successfully downloaded.");
 }
